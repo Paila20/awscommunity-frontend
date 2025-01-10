3870953c7376bc4543c18ee8d225f3b83ec467bc
paila20 <rajarajeswar169@gmail.com>
2025-01-10 18:36:04 +0530

    corrected backendurl

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -25,7 +25,7 @@ export const BlogProvider = ({ children }) => {
     const [mypendingblogs, setMyPendingBlogs] = useState([]);
     const [myrejectedblogs, setMyRejectedBlogs] = useState([]);
 
-    const API_BASE_URL = 'https://backend-blogs-s8uc.onrender.com/'; 
+    const API_BASE_URL = 'https://backend-blogs-s8uc.onrender.com'; 
     const token = localStorage.getItem('token');
 
     const fetchAdminUsers = async () => {
